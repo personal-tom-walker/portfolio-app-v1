0b9251a4d7c02e092daf1393ac5946a403e056da
tom-walker <walkertom1996@gmail.com>
2024-01-28 19:49:02 +0000

    homepage done mobile

--- app/constants/global.tsx
+++ app/constants/global.tsx
@@ -2,3 +2,9 @@ export const breakpoints = {
     desktop: 1024,
     tablet: 768,
 };
+
+export const buttonTypes = {
+    projects: 'projects',
+    about: 'about',
+    experience: 'experience',
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,81 @@
 import { useState } from 'react';
 
 import { mainContentHome } from './constants/mainContent';
+import { buttonTypes } from './constants/global';
 
 import ConnectedCirclesDescendingScale from './components/ConnectedCirclesDescendingScale';
 import MainContent from './components/MainContent';
 
 export default function Home() {
-  const [navClick, setNavClick] = useState(false);
-  const triggerNavClick = () => {
-    console.log('nav click triggered');
-    setNavClick(true);
+  const [navClick, setNavClick] = useState({
+    projects: false,
+    about: false,
+    experience: false,
+  });
+  const triggerNavClick = (type : string) => {
+    setNavClick({ ...navClick, [type]: true});
   };
 
   return (
     <>
       <header className='flex pt-10 pl-10 pr-5 justify-end'>
         <div className='w-[137px]'>
-          <div role='button' onClick={triggerNavClick} className='button'>
-            <span className='button-text w-[100px]'>{'Projects'}</span>
-            <div className={`button-line ${navClick ? 'w-10' : 'w-5'}`}></div>
+          <div
+            role='button'
+            onClick={() => triggerNavClick(buttonTypes.projects)}
+            className='button'
+          >
+            <span className='button-text w-[100px]'>
+              {buttonTypes.projects}
+            </span>
+            <div
+              className={`button-line-horizontal ${
+                navClick.projects ? 'w-10' : 'w-5'
+              }`}
+            ></div>
           </div>
         </div>
       </header>
-      <main className='flex flex-col items-end min-h-screen pb-4 px-8'>
-        <ConnectedCirclesDescendingScale letter={'T'} />
-        <MainContent content={mainContentHome} />
+      <main className='flex flex-col items-end overflow-hidden pb-5 px-10'>
+        <div className='relative flex'>
+          <ConnectedCirclesDescendingScale letter={'T'} />
+        </div>
+        <div className='flex mt-[-160px]'>
+          <MainContent content={mainContentHome} />
+        </div>
       </main>
-      <footer className='flex justify-between pb-10 px-5'></footer>
+      <footer className='absolute bottom-0 w-full h-20 flex justify-between pb-5 px-10'>
+        <div className='w-[100px]'>
+          <div
+            role='button'
+            onClick={() => triggerNavClick(buttonTypes.about)}
+            className='button'
+          >
+            <span className='button-text w-[70px]'>{buttonTypes.about}</span>
+            <div
+              className={`button-line-vertical-short ml-[-6px] ${
+                navClick.about ? 'h-5' : 'h-[10px]'
+              }`}
+            ></div>
+          </div>
+        </div>
+        <div className='w-[120px]'>
+          <div
+            role='button'
+            onClick={() => triggerNavClick(buttonTypes.experience)}
+            className='button'
+          >
+            <span className='button-text w-[120px]'>
+              {buttonTypes.experience}
+            </span>
+            <div
+              className={`button-line-vertical-long ml-[-2px] ${
+                navClick.experience ? 'h-10' : 'h-5'
+              }`}
+            ></div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
