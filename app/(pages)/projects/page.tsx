ac0867675a3fa18724f521592fa1383148b4b72f
tom-walker <walkertom1996@gmail.com>
2024-02-20 17:04:41 +0000

    operator correction

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,7 +1,7 @@
 import ProjectsWrapper from '../../components/PageWrappers/Projects';
 
 export default function Projects() {
-  if (typeof window !== 'undefined') {
+  if (typeof window === 'undefined') {
     return;
   }
   return <ProjectsWrapper />;
