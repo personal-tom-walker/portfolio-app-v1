c7837a68ab7cc1dedfce4c4e4aee007ccd589760
tom-walker <walkertom1996@gmail.com>
2024-02-20 17:01:17 +0000

    fix attempt for window undefined on prerender error for projects page

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,18 +1,8 @@
-'use client';
-
-import { useContext } from 'react';
-
-import { DEVICE_NAMES } from '../../constants/global';
-import { DeviceContext } from '../../hooks/useContext/context';
-
-import ProjectsMobile from '../../components/MobileOnly/Projects';
-import ProjectsDesktop from '../../components/DesktopOnly/Projects';
+import ProjectsWrapper from '../../components/PageWrappers/Projects';
 
 export default function Projects() {
-  const device = useContext(DeviceContext);
-
-  if (device === DEVICE_NAMES.MOBILE) {
-    return <ProjectsMobile />;
+  if (typeof window !== 'undefined') {
+    return;
   }
-  return <ProjectsDesktop />;
+  return <ProjectsWrapper />;
 }
