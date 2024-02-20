f035459c4128d1e425445f11395a48bd509251a3
tom-walker <walkertom1996@gmail.com>
2024-02-20 17:38:07 +0000

    DeviceLayout commented

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,23 +2,23 @@
 // import { DeviceContext } from '../hooks/useContext/context';
 // import useWindowWidth from '../hooks/useWindowWidth';
 
-export default function DeviceLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  //   return (
-  //     <DeviceContext.Provider value={getDeviceContext()}>
-  //       {children}
-  //     </DeviceContext.Provider>
-  //   );
-  // }
-  if (typeof window === 'undefined') {
-    return (
-      <div className='flex items-center justify-center h-screen'>
-        <h2>{'Loading...'}</h2>
-      </div>
-    );
-  }
-  return <>{children}</>
-}
+// export default function DeviceLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <DeviceContext.Provider value={getDeviceContext()}>
+//       {children}
+//     </DeviceContext.Provider>
+//   );
+//   if (typeof window === 'undefined') {
+//     return (
+//       <div className='flex items-center justify-center h-screen'>
+//         <h2>{'Loading...'}</h2>
+//       </div>
+//     );
+//   }
+//   return <>{children}</>
+// }
+  
